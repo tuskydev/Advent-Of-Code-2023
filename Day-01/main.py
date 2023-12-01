db6d895c3277e8541b073affec268fb64cc35132
Mario Fernandez <diddleslip@gmail.com>
2023-12-01 18:18:35 -0500

    fixed syntaxz

--- Day-01/main.py
+++ Day-01/main.py
@@ -1,18 +1,14 @@
-dir = "./Day-01./list.txt"
-handle = open(dir, "r")
+handle = open("./Day-01./list.txt", "r")
 
 total = int()
-
 for line in handle:
   twoDigits = ""
   for char in line[:]:
-    # print("FIRST LOOP",char)
     if char.isdigit():
       twoDigits += char
       break
   for char in line[::-1]:
     if char.isdigit():
-      print("second loop:",char)
       twoDigits += char
       break
 
